4553287b11957d823083c4760cc69a60533a661b
QuangTung97 <quangtung29121997@gmail.com>
2021-12-17 10:54:33 +0700

    Add Response Reader

--- memcache/reader.go
+++ memcache/reader.go
@@ -1,42 +1,124 @@
 package memcache
 
 type responseReader struct {
-	data      []byte
-	size      int
-	lastIndex int
+	data     []byte
+	begin    uint64
+	end      uint64
+	dataMask uint64
 
-	valid  bool
-	cmdEnd int
+	lastPos    uint64
+	waitForEnd uint64
 }
 
-func newResponseReader() *responseReader {
+func newResponseReader(sizeLog int) *responseReader {
 	return &responseReader{
-		data:      make([]byte, 1<<21),
-		lastIndex: 0,
+		data:     make([]byte, 1<<sizeLog),
+		begin:    0,
+		end:      0,
+		dataMask: 1<<sizeLog - 1,
+
+		lastPos: 0,
 	}
+}
+
+func (r *responseReader) getCap() uint64 {
+	return uint64(len(r.data))
 }
 
 func (r *responseReader) recv(data []byte) {
-	copy(r.data, data)
-	r.size += len(data)
+	first := r.getIndex(r.end)
+	copy(r.data[first:], data)
 
-	for i := r.lastIndex; i < r.size-1; i++ {
-		if r.data[i] == '\r' && r.data[i+1] == '\n' {
-			r.valid = true
-			r.cmdEnd = i + 2
-			break
+	max := r.getCap()
+	if first+uint64(len(data)) > max {
+		firstPart := max - first
+		copy(r.data, data[firstPart:])
+	}
+
+	r.end += uint64(len(data))
+}
+
+func (r *responseReader) getIndex(pos uint64) uint64 {
+	return pos & r.dataMask
+}
+
+func (r *responseReader) findFirstNumberPos(start uint64, end uint64) uint64 {
+	for pos := start; pos < end; pos++ {
+		i := r.getIndex(pos)
+		if r.data[i] >= '0' && r.data[i] <= '9' {
+			return pos
 		}
 	}
+	return end
 }
 
-func (r *responseReader) hasNext() bool {
-	return r.valid
+func (r *responseReader) parseIntFrom(start uint64, end uint64) uint64 {
+	result := uint64(0)
+
+	start = r.findFirstNumberPos(start, end)
+	for pos := start; pos < end; pos++ {
+		i := r.getIndex(pos)
+		if r.data[i] < '0' || r.data[i] > '9' {
+			break
+		}
+		num := uint64(r.data[i] - '0')
+		result *= 10
+		result += num
+	}
+
+	return result
 }
 
-func (r *responseReader) responseSize() int {
-	return r.cmdEnd
+func (r *responseReader) isCRLF(pos uint64) bool {
+	return r.data[r.getIndex(pos)] == '\r' && r.data[r.getIndex(pos+1)] == '\n'
+}
+
+func (r *responseReader) isVA(pos uint64) bool {
+	return r.data[r.getIndex(pos)] == 'V' && r.data[r.getIndex(pos+1)] == 'A'
+}
+
+func (r *responseReader) returnIfHasFullResponseData() (int, bool) {
+	if r.waitForEnd <= r.end {
+		r.lastPos = r.waitForEnd
+		r.waitForEnd = 0
+		return int(r.lastPos - r.begin), true
+	}
+	return 0, false
+}
+
+func (r *responseReader) getNext() (int, bool) {
+	if r.waitForEnd > 0 {
+		return r.returnIfHasFullResponseData()
+	}
+
+	for pos := r.lastPos; pos+1 < r.end; pos++ {
+		if !r.isCRLF(pos) {
+			continue
+		}
+
+		r.lastPos = pos + 2
+		if r.isVA(r.begin) {
+			dataLen := r.parseIntFrom(r.begin+2, pos)
+			r.waitForEnd = r.lastPos + dataLen + 2
+			return r.returnIfHasFullResponseData()
+		}
+		return int(r.lastPos - r.begin), true
+	}
+	if r.end >= r.begin+2 {
+		r.lastPos = r.end - 2
+	}
+	return 0, false
 }
 
 func (r *responseReader) readData(data []byte) {
-	copy(data, r.data)
+	n := r.lastPos - r.begin
+	first := r.getIndex(r.begin)
+	max := r.getCap()
+
+	copy(data, r.data[first:])
+	if first+n > max {
+		firstPart := max - first
+		copy(data[firstPart:], r.data)
+	}
+	r.begin = r.lastPos
 }
--- memcache/reader_test.go
+++ memcache/reader_test.go
@@ -5,18 +5,137 @@ import (
 	"testing"
 )
 
-func TestResponseReader(t *testing.T) {
-	r := newResponseReader()
+func TestResponseReader_Empty(t *testing.T) {
+	r := newResponseReader(10)
+	size, ok := r.getNext()
+	assert.Equal(t, false, ok)
+	assert.Equal(t, 0, size)
+}
+
+func TestResponseReader_Normal(t *testing.T) {
+	r := newResponseReader(10)
 
 	r.recv([]byte("abcd\r\nxxx\r\n"))
 
-	assert.Equal(t, true, r.hasNext())
-
-	size := len("abcd\r\n")
-	assert.Equal(t, size, r.responseSize())
+	size, ok := r.getNext()
+	assert.Equal(t, true, ok)
+	assert.Equal(t, len("abcd\r\n"), size)
 
 	data := make([]byte, size)
 	r.readData(data)
-
 	assert.Equal(t, []byte("abcd\r\n"), data)
+
+	size, ok = r.getNext()
+	assert.Equal(t, true, ok)
+	assert.Equal(t, len("xxx\r\n"), size)
+
+	data = make([]byte, size)
+	r.readData(data)
+	assert.Equal(t, []byte("xxx\r\n"), data)
+
+	size, ok = r.getNext()
+	assert.Equal(t, false, ok)
+	assert.Equal(t, 0, size)
+
+	size, ok = r.getNext()
+	assert.Equal(t, false, ok)
+	assert.Equal(t, 0, size)
+}
+
+func TestResponseReader_Missing_LF(t *testing.T) {
+	r := newResponseReader(10)
+
+	r.recv([]byte("abcd\r"))
+
+	size, ok := r.getNext()
+	assert.Equal(t, false, ok)
+	assert.Equal(t, 0, size)
+
+	r.recv([]byte("\n"))
+
+	size, ok = r.getNext()
+	assert.Equal(t, true, ok)
+	assert.Equal(t, len("abcd\r\n"), size)
+
+	data := make([]byte, size)
+	r.readData(data)
+	assert.Equal(t, []byte("abcd\r\n"), data)
+
+	size, ok = r.getNext()
+	assert.Equal(t, false, ok)
+	assert.Equal(t, 0, size)
+
+	size, ok = r.getNext()
+	assert.Equal(t, false, ok)
+	assert.Equal(t, 0, size)
+}
+
+func TestResponseReader_Wrap_Around(t *testing.T) {
+	r := newResponseReader(3)
+
+	r.recv([]byte("abcd\r\n"))
+
+	size, ok := r.getNext()
+	assert.Equal(t, true, ok)
+	assert.Equal(t, len("abcd\r\n"), size)
+
+	data := make([]byte, size)
+	r.readData(data)
+	assert.Equal(t, []byte("abcd\r\n"), data)
+
+	r.recv([]byte("xxx\r\n"))
+	size, ok = r.getNext()
+	assert.Equal(t, true, ok)
+	assert.Equal(t, len("xxx\r\n"), size)
+
+	data = make([]byte, size)
+	r.readData(data)
+	assert.Equal(t, []byte("xxx\r\n"), data)
+
+	size, ok = r.getNext()
+	assert.Equal(t, false, ok)
+	assert.Equal(t, 0, size)
+
+	size, ok = r.getNext()
+	assert.Equal(t, false, ok)
+	assert.Equal(t, 0, size)
+}
+
+func TestResponseReader_With_Value_Return(t *testing.T) {
+	r := newResponseReader(10)
+
+	r.recv([]byte("VA 13\r\nAA\r\n"))
+
+	size, ok := r.getNext()
+	assert.Equal(t, false, ok)
+	assert.Equal(t, 0, size)
+
+	r.recv([]byte("123456789\r\nVA"))
+
+	expected := "VA 13\r\nAA\r\n123456789\r\n"
+
+	size, ok = r.getNext()
+	assert.Equal(t, true, ok)
+	assert.Equal(t, len(expected), size)
+
+	data := make([]byte, len(expected))
+	r.readData(data)
+	assert.Equal(t, []byte(expected), data)
+
+	size, ok = r.getNext()
+	assert.Equal(t, false, ok)
+
+	// SECOND CYCLE
+
+	r.recv([]byte("   4  \r\nabcd\r\n"))
+
+	expected = "VA   4  \r\nabcd\r\n"
+
+	size, ok = r.getNext()
+	assert.Equal(t, true, ok)
+	assert.Equal(t, len(expected), size)
+
+	data = make([]byte, len(expected))
+	r.readData(data)
+	assert.Equal(t, []byte(expected), data)
 }
