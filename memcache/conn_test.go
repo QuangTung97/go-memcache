82e2f74f4794e7c021739686025d44085ed6f4a8
QuangTung97 <quangtung29121997@gmail.com>
2021-12-23 17:43:24 +0700

    With Set

--- memcache/conn_test.go
+++ memcache/conn_test.go
@@ -2,6 +2,7 @@ package memcache
 
 import (
 	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 )
 
@@ -14,4 +15,40 @@ func TestConn_Simple_Get_Miss(t *testing.T) {
 	cmd1.wait()
 
 	assert.Equal(t, "EN\r\n", string(cmd1.data))
+}
+
+func TestConn_Get_Multi_Keys_All_Missed(t *testing.T) {
+	c, err := newConn("localhost:11211")
+	assert.Equal(t, nil, err)
+
+	cmd1 := newCommandFromString("mg key01 v\r\nmg key02 v\r\nmg key03 v\r\n")
+	cmd1.cmdCount = 3
+	c.pushCommand(cmd1)
+	cmd1.wait()
+
+	assert.Equal(t, "EN\r\nEN\r\nEN\r\n", string(cmd1.data))
+}
+
+func TestConn_Set_Get(t *testing.T) {
+	c, err := newConn("localhost:11211")
+	assert.Equal(t, nil, err)
+
+	commands := []string{
+		"ms key01 4\r\nABCD\r\n",
+		"mg key01 v\r\n",
+		"flush_all\r\n",
+	}
+
+	cmd := newCommandFromString(strings.Join(commands, ""))
+	cmd.cmdCount = 3
+	c.pushCommand(cmd)
+	cmd.wait()
+
+	results := []string{
+		"HD\r\n",
+		"VA 4\r\nABCD\r\n",
+		"OK\r\n",
+	}
+
+	assert.Equal(t, strings.Join(results, ""), string(cmd.data))
 }
