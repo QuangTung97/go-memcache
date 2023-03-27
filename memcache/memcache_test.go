9c535461b544f888ac53c2829df005227d56b909
QuangTung97 <quangtung29121997@gmail.com>
2023-03-27 17:15:24 +0700

    Set Error Property Based Test

--- memcache/memcache_test.go
+++ memcache/memcache_test.go
@@ -193,3 +193,100 @@ func TestMemcache_Consistent_Cache(t *testing.T) {
 	err = client.Close()
 	assert.Equal(t, nil, err)
 }
+
+func TestMemcache_Object_Is_Too_Big(t *testing.T) {
+	client, err := New("localhost:11211", 2)
+	assert.Equal(t, nil, err)
+
+	for i := 0; i < 5; i++ {
+		testCacheObjectIsTooBigContinuing(t, client)
+		if t.Failed() {
+			fmt.Println("LOOP:", i)
+			return
+		}
+	}
+
+	err = client.Close()
+	assert.Equal(t, nil, err)
+}
+
+func testCacheObjectIsTooBigContinuing(_ *testing.T, client *Client) {
+	const numThreads = 10
+	const numKeys = 20
+
+	var wg sync.WaitGroup
+	wg.Add(numThreads)
+
+	type cacheValue struct {
+		keyNum uint64
+		size   int
+	}
+
+	var mut sync.Mutex
+	nextNum := uint64(0)
+	values := make([]cacheValue, 0, numThreads*numKeys)
+	results := map[uint64]error{}
+
+	getNextValue := func() cacheValue {
+		size := rand.Intn(2 << 20) // 0 => 2MB
+
+		mut.Lock()
+		nextNum++
+		val := cacheValue{
+			keyNum: nextNum,
+			size:   size,
+		}
+		values = append(values, val)
+		mut.Unlock()
+
+		return val
+	}
+
+	getKey := func(num uint64) string {
+		return fmt.Sprintf("KEY%08d", num)
+	}
+
+	const headerSize = 56     // 8 * 7
+	const paddingSize = 1 + 2 // 1 null terminated key, 2 for \r\n data
+
+	for th := 0; th < numThreads; th++ {
+		go func() {
+			defer wg.Done()
+
+			for i := 0; i < numKeys; i++ {
+				val := getNextValue()
+				key := getKey(val.keyNum)
+
+				dataSize := val.size - headerSize - len(key) - paddingSize
+
+				data := repeatBytes('A', dataSize)
+				pipe := client.Pipeline()
+				_, err := pipe.MSet(key, data, MSetOptions{})()
+
+				mut.Lock()
+				results[val.keyNum] = err
+				mut.Unlock()
+
+				pipe.Finish()
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	for _, val := range values {
+		err := results[val.keyNum]
+
+		if val.size <= 1<<20 {
+			if err != nil {
+				fmt.Println("VALUE:", val.keyNum, val.size, err)
+				panic("should not error when object is not too big")
+			}
+		} else {
+			if err != ErrObjectTooBig {
+				fmt.Println("VALUE:", val.keyNum, val.size, err)
+				panic("should error too big")
+			}
+		}
+	}
+}
