590250838ed74a1db1deab24401d68aa26038d08
QuangTung97 <quangtung29121997@gmail.com>
2023-10-09 09:44:26 +0700

    Add Byte Pool

---
+++ memcache/pool.go
@@ -0,0 +1,68 @@
+package memcache
+
+import (
+	"math/bits"
+	"sync"
+)
+
+const (
+	poolGetDataBaseSizeLog = 6
+	poolGetDataBaseSize    = 1 << poolGetDataBaseSizeLog
+	poolNumLevels          = 12
+	poolMaxBytes           = poolGetDataBaseSize * (1 << (poolNumLevels - 1))
+)
+
+func initGetDataPools() []*sync.Pool {
+	result := make([]*sync.Pool, poolNumLevels)
+	for i := range result {
+		level := i
+		result[i] = &sync.Pool{
+			New: func() any {
+				return make([]byte, 0, 1<<(poolGetDataBaseSizeLog+level))
+			},
+		}
+	}
+	return result
+}
+
+var getDataPools = initGetDataPools()
+
+func getByteSlice(size uint64) []byte {
+	if size == 0 {
+		return nil
+	}
+	if size > poolMaxBytes {
+		return make([]byte, size)
+	}
+
+	numBits := bits.Len32(uint32(size - 1))
+	level := numBits - poolGetDataBaseSizeLog
+	if numBits < poolGetDataBaseSizeLog {
+		level = 0
+	}
+
+	result := getDataPools[level].Get().([]byte)
+	return result[:size]
+}
+
+func ReleaseGetResponseData(data []byte) {
+	capacity := cap(data)
+	if capacity == 0 {
+		return
+	}
+	if capacity > poolMaxBytes {
+		return
+	}
+
+	numBits := bits.Len32(uint32(capacity - 1))
+	if capacity != (1 << numBits) {
+		return
+	}
+	if numBits < poolGetDataBaseSizeLog {
+		return
+	}
+	level := numBits - poolGetDataBaseSizeLog
+
+	data = data[:0]
+	getDataPools[level].Put(data)
+}
---
+++ memcache/pool_test.go
@@ -0,0 +1,108 @@
+package memcache
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPoolMaxSize(t *testing.T) {
+	assert.Equal(t, 64, poolGetDataBaseSize)
+	assert.Equal(t, 128*1024, poolMaxBytes)
+}
+
+func TestPool_GetByteSlice(t *testing.T) {
+	t.Run("normal", func(t *testing.T) {
+		x := getByteSlice(64)
+		assert.Equal(t, 64, len(x))
+		assert.Equal(t, 64, cap(x))
+	})
+
+	t.Run("size < 64", func(t *testing.T) {
+		x := getByteSlice(63)
+		assert.Equal(t, 63, len(x))
+		assert.Equal(t, 64, cap(x))
+
+		x = getByteSlice(65)
+		assert.Equal(t, 65, len(x))
+		assert.Equal(t, 128, cap(x))
+
+		x = getByteSlice(127)
+		assert.Equal(t, 127, len(x))
+		assert.Equal(t, 128, cap(x))
+
+		x = getByteSlice(127)
+		assert.Equal(t, 127, len(x))
+		assert.Equal(t, 128, cap(x))
+
+		x = getByteSlice(256)
+		assert.Equal(t, 256, len(x))
+		assert.Equal(t, 256, cap(x))
+	})
+
+	t.Run("zero", func(t *testing.T) {
+		x := getByteSlice(0)
+		assert.Equal(t, 0, len(x))
+		assert.Equal(t, 0, cap(x))
+
+		x = getByteSlice(1)
+		assert.Equal(t, 1, len(x))
+		assert.Equal(t, 64, cap(x))
+	})
+
+	t.Run("bigger than max", func(t *testing.T) {
+		x := getByteSlice(128*1024 + 1)
+		assert.Equal(t, 128*1024+1, len(x))
+		assert.Equal(t, 128*1024+1, cap(x))
+	})
+
+	t.Run("get and put and get", func(t *testing.T) {
+		x := getByteSlice(128)
+		ReleaseGetResponseData(x)
+
+		x = getByteSlice(65)
+		assert.Equal(t, 65, len(x))
+		assert.Equal(t, 128, cap(x))
+	})
+
+	t.Run("release nil", func(t *testing.T) {
+		ReleaseGetResponseData(nil)
+
+		x := getByteSlice(1)
+		assert.Equal(t, 1, len(x))
+		assert.Equal(t, 64, cap(x))
+	})
+
+	t.Run("release bigger than max", func(t *testing.T) {
+		ReleaseGetResponseData(make([]byte, 128*1024+1))
+
+		x := getByteSlice(128*1024 - 1)
+		assert.Equal(t, 128*1024-1, len(x))
+		assert.Equal(t, 128*1024, cap(x))
+	})
+
+	t.Run("release not align", func(t *testing.T) {
+		ReleaseGetResponseData(make([]byte, 63))
+
+		x := getByteSlice(63)
+		assert.Equal(t, 63, len(x))
+		assert.Equal(t, 64, cap(x))
+	})
+
+	t.Run("release too small", func(t *testing.T) {
+		ReleaseGetResponseData(make([]byte, 32))
+
+		x := getByteSlice(32)
+		assert.Equal(t, 32, len(x))
+		assert.Equal(t, 64, cap(x))
+	})
+}
+
+func Benchmark_Pool_Get_And_Put(b *testing.B) {
+	sum := 0
+	for n := 0; n < b.N; n++ {
+		x := getByteSlice(127)
+		sum += len(x)
+		ReleaseGetResponseData(x)
+	}
+}
